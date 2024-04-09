d51b88d09ab7f5f0b018a4c093d8a1bc07bbf4b5
gguiberteau <gguibert@alumnos.unex.es>
2024-04-09 14:05:24 +0200

    Ranking mejorado y Tika funcional y documentado para txt, pdf y html

--- src/TikaParsers.java
+++ src/TikaParsers.java
@@ -2,7 +2,6 @@ import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.html.HtmlParser;
-import org.apache.tika.parser.microsoft.ooxml.OOXMLParser;
 import org.apache.tika.parser.pdf.PDFParser;
 import org.apache.tika.parser.txt.TXTParser;
 import org.apache.tika.sax.BodyContentHandler;
